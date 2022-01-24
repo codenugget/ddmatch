95006fcdd082fba50c8c5cd295ddf7087e7204e0
David Löfstrand <davidlofstrand@gmail.com>
2022-01-24 19:15:56 +0100

    Updates .gitignore, creates runtime folder on build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -x
 BUILD_ROOT_DIR=`realpath ..`
+
+if [ ! -d ../runtime ]; then
+  mkdir ../runtime
+fi
+
 pushd .
 if [ ! -d ../vcpkg ]; then
   pushd ..
@@ -11,7 +16,7 @@ if [ ! -d ../vcpkg ]; then
   popd
 fi
 
-if ! [ -d build ]; then
+if [ ! -d build ]; then
   mkdir -p build
   cd build
   cmake $BUILD_ROOT_DIR -DCMAKE_BUILD_TYPE=Release -DCONTAINER_BUILD_HACK=False
