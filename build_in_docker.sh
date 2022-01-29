7169def3e6e6e44ec17d1a779b8f2abb80e24000
David Löfstrand <davidlofstrand@gmail.com>
2022-01-29 11:24:51 +0100

    Moved output from docker build to separate folder.

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
-if [ ! -d runtime ]; then
-  mkdir runtime
+if [ ! -d docker_build ]; then
+  mkdir docker_build
 fi
-
+rm -f docker_build/*
 cur_dir=`pwd`
 
-docker run -v ${cur_dir}/runtime:/app/runtime --rm ddmatch_build
+docker run -v ${cur_dir}/docker_build:/app/runtime --rm ddmatch_build
